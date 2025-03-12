f904e61dcfa606a726887298cd06d50fa831241d
Dustin Hofmann <dustin.hofmann@iu-study.org>
2025-03-12 23:21:22 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dustin-hofmann/.github.io',
+  base: '/rolfing', // z.B. '/rolfing-website' (wenn das Ihr Repository-Name ist)
+  output: 'static',
+});
